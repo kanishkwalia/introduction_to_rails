f83f890738da9e214c9cfee61becf2bd753add12
massud <massudgnan@gmail.com>
2015-05-06 14:27:47 +0100

    omniAuth login

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
-  devise_for :users
+                  
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do
     resources :reviews
+  end
     root to: "restaurants#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -58,5 +59,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  end
+
 end
